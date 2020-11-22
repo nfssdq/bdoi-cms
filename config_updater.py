19096a25b859a1485a20cdfad05512c66e3bc17c
nfssdq <6448429+nfssdq@users.noreply.github.com>
2020-11-23 03:39:23 +0600

    Add files via upload
    updates

--- config_updater.py
+++ config_updater.py
@@ -1,4 +1,7 @@
+#!python3
+
 from google.cloud import storage
+from google.cloud import secretmanager
 from googleapiclient import discovery
 import json
 import sys
@@ -8,19 +11,26 @@ import subprocess
 PROJECT_NAME = 'bdoi-cms'
 SQL_INSTANCE_NAME = 'bdoi'
 CONFIG_FILE_NAME = 'cms.conf'
+CONTEST_DB_PASSWORD_SECRET_ID = 'cmsdb-password'
+TELEGRAM_BOT_TOKEN_SECRET_ID = 'telegrambot-token'
+TELEGRAM_BOT_CHAT_SECRET_ID = 'telegrambot-chat-id'
 
 compute = discovery.build('compute', 'v1')
 sqladmin = discovery.build('sqladmin', 'v1beta4')
+secretmanagerclient = secretmanager.SecretManagerServiceClient()
 bucket = storage.Client(project=PROJECT_NAME).bucket('contestdata')
 zones = ['asia-southeast1-a', 'asia-southeast1-b',
          'asia-southeast1-c', 'asia-south1-a']
 
 
-def list_servers(servertype=None):
+def list_servers(servertype):
+    # returns a list of server
+    # each server is a list of three items [ip, name, zone].
     servers = []
     for zone in zones:
         instances = compute.instances().list(
-            project=PROJECT_NAME, zone=zone).execute()
+            project=PROJECT_NAME, zone=zone,
+            filter='labels.type={}'.format(servertype)).execute()
         if 'items' not in instances:
             continue
         for item in instances['items']:
@@ -39,45 +49,55 @@ def load_conf():
 
 
 def save_conf(conf):
-    bucket.blob(CONFIG_FILE_NAME).upload_from_string(json.dumps(conf))
+    bucket.blob(CONFIG_FILE_NAME).upload_from_string(
+        json.dumps(conf, indent=4))
 
 
-def process_workers(conf, admin, worker, ranking):
+def get_secret(secret_id):
+    secret = secretmanagerclient.access_secret_version(
+        name='projects/{}/secrets/{}/versions/latest'.format(
+            PROJECT_NAME, secret_id))
+    return secret.payload.data.decode('utf-8')
+
+
+def populate_workers(conf, admin, workers, rankingservers):
     services = conf['core_services']
     other_services = conf['other_services']
+
+    # admin server
     services['LogService'] = [[admin[0], 29000]]
     services['AdminWebServer'] = [[admin[0], 21100]]
     services['ProxyService'] = [[admin[0], 28600]]
     services['EvaluationService'] = [[admin[0], 25000]]
-    services['ResourceService'] = []
-    services['ScoringService'] = []
-    services['Checker'] = []
-    services['Worker'] = []
-    services['ContestWebServer'] = []
-    conf['rankings'] = []
-    other_services['TestFileCacher'] = []
-    conf['contest_listen_address'] = []
-    conf['contest_listen_port'] = []
-
-    services['ResourceService'].append([admin[0], 28000])
+    services['ResourceService'] = [[admin[0], 28000]]
     conf['admin_listen_address'] = admin[0]
 
-    for server in worker:
-        services['ResourceService'].append([server[0], 28000])
-        services['ScoringService'].append([server[0], 28500])
-        services['Checker'].append([server[0], 22000])
-        services['Worker'].append([server[0], 26000])
-        services['Worker'].append([server[0], 26001])
-        services['Worker'].append([server[0], 26002])
-        services['Worker'].append([server[0], 26003])
-        services['ContestWebServer'].append([server[0], 21000])
-        conf['contest_listen_address'].append(server[0])
-        conf['contest_listen_port'].append(8888)
-        other_services['TestFileCacher'].append([server[0], 27501])
+    # contest server workers
+    services['ResourceService'].extend(
+        [[server[0], 28000] for server in workers])
+    services['ScoringService'] = [[server[0], 28500] for server in workers]
+    services['Checker'] = [[server[0], 22000] for server in workers]
+    services['Worker'] = [[server[0], 26000] for server in workers]
+    services['Worker'].extend([[server[0], 26001] for server in workers])
+    services['Worker'].extend([[server[0], 26002] for server in workers])
+    services['Worker'].extend([[server[0], 26003] for server in workers])
+    other_services['TestFileCacher'] = [[server[0], 27501]
+                                        for server in workers]
 
-    for server in ranking:
+    # contest web service
+    services['ContestWebServer'] = [[server[0], 21000] for server in workers]
+    conf['contest_listen_address'] = [server[0] for server in workers]
+    conf['contest_listen_port'] = [8888 for server in workers]
+
+    # ranking servers
+    conf['rankings'] = []
+    for server in rankingservers:
         conf['rankings'].append(
             "http://ranking:jdmnpn@{}:8890/".format(server[0]))
+
+    # telegram config
+    conf['telegram_bot_token'] = get_secret(TELEGRAM_BOT_TOKEN_SECRET_ID)
+    conf['telegram_bot_chat_id'] = int(get_secret(TELEGRAM_BOT_CHAT_SECRET_ID))
 
 
 def make_commands(cid):
@@ -88,34 +108,52 @@ def make_commands(cid):
         .format(cid)]
 
 
-def reload(instance, zone, cid):
-    for cmd in make_commands(cid):
-        print(subprocess.run(
+def reload(instance, zone, commands):
+    print("reloading {} in zone {}".format(instance, zone))
+    for cmd in commands:
+        result = subprocess.run(
             ["gcloud compute ssh {} --zone={} --command=\"{}\"".format(
-                instance, zone, cmd)], stdout=subprocess.PIPE, shell=True))
+                instance, zone, cmd)], stdout=subprocess.PIPE, shell=True)
+        if result.returncode != 0:
+            print("error running {}, stdout: {}".format(
+                result.args, result.stdout))
 
 
-def update_db(conf):
+def populate_db(conf):
     instance = sqladmin.instances().get(
         project=PROJECT_NAME, instance=SQL_INSTANCE_NAME).execute()
     ip = instance['ipAddresses'][0]['ipAddress']
-    conf['database'] = 'postgresql+psycopg2://cmsuser:sqAM5BY8U8VX@{}:5432/cmsdb'.format(ip)
+    conf['database'] = \
+        'postgresql+psycopg2://cmsuser:{}@{}:5432/cmsdb'.format(
+            get_secret(CONTEST_DB_PASSWORD_SECRET_ID), ip)
 
 
-def controller(cid):
-    admin = list_servers('adminserver')
-    worker = list_servers('contestserver')
+def controller(cid=None):
+    admin = list_servers('adminserver')[0]
+    workers = list_servers('contestserver')
     ranking = list_servers('rankingserver')
+
     conf = load_conf()
-    update_db(conf)
-    process_workers(conf, admin[0], worker, ranking)
-    print(json.dumps(conf))
+    populate_db(conf)
+    populate_workers(conf, admin, workers, ranking)
+    print(json.dumps(conf, indent=4))
     save_conf(conf)
-    for server in worker:
-        reload(server[1], server[2], cid)
-    for cmd in make_commands(cid):
-        print(subprocess.run([cmd], stdout=subprocess.PIPE, shell=True))
+
+    if cid is None:
+        return
+
+    commands = make_commands(cid)
+    [reload(server[1], server[2], commands) for server in workers]
+    [reload(server[1], server[2], commands) for server in ranking]
+
+    commands = [
+        'sudo screen -XS cmsLogService quit',
+        'sudo screen -S cmsLogService -d -m cmsLogService'] + commands
+    reload(admin[1], admin[2], commands)
 
 
 if __name__ == "__main__":
-    controller(sys.argv[1])
+    if len(sys.argv) > 1:
+        controller(cid=sys.argv[1])
+    else:
+        controller()
